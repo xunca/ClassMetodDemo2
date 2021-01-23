e0c8648c3b885527cbe3cd0dea3485606af0424a
sbayram93 <mikrozom@gmail.com>
2021-01-23 16:35:27 +0300

    Add project files.

---
+++ MusteriTakip/MusteriManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.TC + " TC'li " + musteri.Ad + " " + musteri.Soyad + " adlı kişi eklenmiştir.");
+        }
+        public void Listele(Musteri musteri)
+        {
+            Console.WriteLine("Listelenmiştir.");
+        }
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine(musteri.TC + " TC'li " + musteri.Ad + " " + musteri.Soyad + " adlı müşteri silinmiştir.");
+        }
+
+    }
+}
+
---
+++ MusteriTakip/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri client1 = new Musteri();
+            client1.TC = 12345678901;
+            client1.Ad = "Sergen";
+            client1.Soyad = "Yalçın";
+
+            Musteri client2 = new Musteri();
+            client2.TC = 34343433111;
+            client2.Ad = "İlhan";
+            client2.Soyad = "Mansız";
+
+            Musteri client3 = new Musteri();
+            client3.TC = 55445432321;
+            client3.Ad = "Atilla";
+            client3.Soyad = "Yeşilada";
+
+            
+
+            Console.WriteLine("Listelenmek istenen müşteriler");
+            Musteri[] musteriler = new Musteri[] { client1, client2, client3 };
+
+            foreach (var Musteri in musteriler)
+            {
+                Console.WriteLine(Musteri.TC + " " + Musteri.Ad + " " + Musteri.Soyad);
+            }
+
+
+            Console.WriteLine("Eklenmek istenen müşteriler");
+            MusteriManager musteri = new MusteriManager();
+            musteri.Ekle(client1);
+            musteri.Ekle(client2);
+            musteri.Ekle(client3);
+
+
+
+            Console.WriteLine("Silinmek istenen müşteriler");
+            musteri.Sil(client3);
+
+            Console.WriteLine("Güncellenmek istenen müşteriler");
+            
+
+        }
+    }
+}
